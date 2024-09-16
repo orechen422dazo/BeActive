04300443626a1e58fe5382bd555a94d57995de1b
orechen422dazo <jboy422hobby@gmail.com>
2024-09-17 06:42:46 +0900

    Initial Commit

---
+++ BeActive/BeActiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  BeActiveApp.swift
+//  BeActive
+//
+//  Created by 橋本純一 on 2024/09/17.
+//
+
+import SwiftUI
+
+@main
+struct BeActiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
